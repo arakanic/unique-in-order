0249f7e7afeb40c76a5ccde89a10a5896a9f8dfb
jiara <ji.arakani@gmail.com>
2022-05-27 17:04:36 -0400

    first commit

---
+++ uniqueinorder.js
@@ -0,0 +1,8 @@
+// This function takes a string sequence and returns a list of characters which preserves the original order while no character repeats itself consecutively. For example:
+
+// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+// uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+const uniqueInOrder = (iterable) =>
+  [...iterable].filter((a, i) => a != [...iterable][i + 1])
